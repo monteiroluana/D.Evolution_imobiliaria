6d86ec7a94d23a72bfd13691e1f4c3bc8bd98a80
Eduardo Teixeira Monteiro <edutm87@hotmail.com>
2018-04-15 13:58:01 -0300

    adicionado outra pagina exemplo

---
+++ ImobiliariaTest/src/main/java/br/com/evolution/servlets/PaginaQualquerServlet.java
@@ -0,0 +1,42 @@
+package br.com.evolution.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = { "/pagina-qualquer" })
+public class PaginaQualquerServlet extends HttpServlet{
+	
+	private static final long serialVersionUID = 1L;
+	 
+	   public PaginaQualquerServlet() {
+	       super();
+	   }
+	 
+	   @Override
+	   protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	           throws ServletException, IOException {
+		   //Caso a requisição foir GET cairá aqui
+		   
+		   //indica para qual jsp que vai
+		   RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/pages/pagina-qualquer.jsp");
+	        
+		   //aqui vai codigo doque sera feito
+		   
+		   
+		   //encaminha requisição
+		   dispatcher.forward(request, response);
+	        
+	   }
+	   
+	   @Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//caso a requisição for POST cairá aqui
+		super.doPost(req, resp);
+	}
+}
